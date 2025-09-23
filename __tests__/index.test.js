a86bc90978e740b0cc3c8cccaf8fc967a15ea8c1
zhenia-chugaev <zhenia.chugaev@bk.ru>
2025-09-23 16:15:10 +0200

    Refactor test case

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,7 +6,8 @@ import init from '@hexlet/react-todo-app-with-backend';
 import { screen, render } from '@testing-library/react';
 
 test('initiating the app', () => {
-  render(init({}));
+  const initialState = {};
+  render(init(initialState));
   expect(screen.getByText(/hexlet todos/i)).toBeInTheDocument();
   expect(screen.getByRole('heading', { name: /lists/i })).toBeInTheDocument();
   expect(screen.getByRole('heading', { name: /tasks/i })).toBeInTheDocument();
