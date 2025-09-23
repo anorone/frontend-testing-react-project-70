bf34dce00079ec2ba82d1c322940151d7f324aa0
zhenia-chugaev <zhenia.chugaev@bk.ru>
2025-09-23 15:00:20 +0200

    Add test case

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,9 +1,13 @@
 // @ts-check
 
-import { test } from '@jest/globals';
+import { expect, test } from '@jest/globals';
 
 import init from '@hexlet/react-todo-app-with-backend';
+import { screen, render } from '@testing-library/react';
 
 test('initiating the app', () => {
-  init({});
+  render(init({}));
+  expect(screen.getByText(/hexlet todos/i)).toBeInTheDocument();
+  expect(screen.getByRole('heading', { name: /lists/i })).toBeInTheDocument();
+  expect(screen.getByRole('heading', { name: /tasks/i })).toBeInTheDocument();
 });
