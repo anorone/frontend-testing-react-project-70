4d719ac429a5b5b94153dd3c28ee612bd2cb27f0
zhenia-chugaev <zhenia.chugaev@bk.ru>
2025-10-11 16:32:49 +0200

    Collect app coverage

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,12 @@ const config = {
   setupFiles: ['<rootDir>/setupEnv.js'],
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
   coverageProvider: 'v8',
+  collectCoverageFrom: [
+    "<rootDir>/node_modules/@hexlet/react-todo-app-with-backend/src/**/*.{jsx,js}",
+  ],
+  coveragePathIgnorePatterns: [
+    'node_modules/(?!@hexlet/react-todo-app-with-backend/)',
+  ],
   testEnvironment: 'jest-fixed-jsdom',
   transformIgnorePatterns: [
     'node_modules/(?!(@hexlet/react-todo-app-with-backend|until-async)/)',
